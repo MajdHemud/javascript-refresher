068edf27367f81df940dcb37738483aa705b12d1
Majd Hemud <majd.hemud@gmail.com>
2024-01-13 16:32:35 +0000

    Revisiting Variables & Values

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,19 @@
 //import defaultApiKey from "./util.js";
-import { apiKey1, apiKey2 as key2 } from "./util.js";
+// import { apiKey1, apiKey2 as key2 } from "./util.js";
 // import { apiKey1 } from "./util.js";
 // import * as util from "./util.js";
 
 // console.log(util.default);
-console.log(apiKey1);
-console.log(key2);
+// console.log(apiKey1);
+// console.log(key2);
 // console.log(apiKey2);
 // console.log(util.apiKey2);
+
+let userName = "Majd Hemud";
+let age = 27;
+
+const ssn = "123-45-6789";
+
+console.log(userName);
+console.log(age);
+console.log(ssn);
